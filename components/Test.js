d2efec5d1ca9b4a190df577de8e86cfa081ba92d
Cholago <cholawilson@yahoo.com>
2020-08-29 22:33:42 +0300

    third commit

--- components/Test.js
+++ components/Test.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, Button, View } from 'react-native';
 
 export default function Test() {
+    const [count, setCount] = useState(0);
     return (
         <View style={styles.container}>
             <Text style={styles.textColor}>Hurrar my first react app</Text>
+            <Text style={styles.countText}>Number is {count}</Text>
+            <Button style={styles.incrementButton} title="Increment"
+                onPress={() => setCount(count + 1)} />
+            <Button style={styles.decrementButton} title="Decrement"
+                onPress={() => setCount(count - 1)} />
             <StatusBar style="auto" />
         </View>
     );
@@ -21,6 +27,18 @@ const styles = StyleSheet.create({
     textColor: {
         color: 'green',
         fontSize: 30
-
+    },
+    countText: {
+        color: 'red',
+        fontSize: 20
+    }
+    ,
+    incrementButton: {
+        margin: 20,
+        padding: 20
+    },
+    decrementButton: {
+        margin: 20,
+        backgroundColor: 'red'
     }
 });
